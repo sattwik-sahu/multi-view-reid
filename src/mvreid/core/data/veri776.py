5af610a58c4742cbe62cdfab00f01b43dce8b48c
techaadii <adityasinofficial@gmail.com>
2026-07-22 00:07:49 +0530

    feat[dataset]: Added the dataset class

---
+++ src/mvreid/core/data/veri776.py
@@ -0,0 +1,11 @@
+from mvreid.core._typing import ReidSample
+from mvreid.core.data.base import MultiViewReidDataset
+
+
+class Veri776Dataet(MultiViewReidDataset[int, int]):
+    """
+    Veri776 dataset implementation for multiview reid
+    Class inherits from the MultiViewReidDataset class
+    Args:
+        TODO: Write the deatiled arguments here
+    """
