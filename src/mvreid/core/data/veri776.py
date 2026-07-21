df430dba8b7995434f10c14effc139c832dab293
techaadii <adityasinofficial@gmail.com>
2026-07-22 01:45:45 +0530

    feat[dataset]: Improved the dataset parsing function

--- src/mvreid/core/data/veri776.py
+++ src/mvreid/core/data/veri776.py
@@ -45,10 +45,30 @@ class Veri776Dataet(MultiViewReidDataset[int, int]):
         self.pid_map = {old: old for old in self.entity_ids}
 
     def _parse_dataset(self) -> None:
-        pass
+        """Parses file name and groups them by PID and CamID"""
+        if not self.data_path.exists():
+            raise FileNotFoundError(f"Directory not found:,{self.data_path}")
+        image_paths: list = list(self.data_path.glob.glob("*.jpg"))
 
-    def __len__():
-        pass
+        for path in image_paths:
+            filename = path.stem
+            parts = filename.split("_")
+
+            if len(parts) < 2:
+                continue
+            pid = len(parts[0])
+
+            if pid <= 0:
+                continue
+
+            cam_str = parts[1]
+            camid = int(cam_str[1:]) if cam_str.startswith("c") else int(cam_str)
+
+            self._samples_by_id[pid].append((path, camid))
+
+    def __len__(self) -> int:
+        """Returns the number of unique vehicle entities"""
+        return len(self.entity_ids)
 
     def __getitem__():
         pass
