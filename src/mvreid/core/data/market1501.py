987b706d94615fe7433a996e6f639a6014bcf09e
Sattwik Kumar Sahu <sattwik21@iiserb.ac.in>
2026-07-21 16:24:36 +0530

    feat[dataset]: fixed boilerplate code

--- src/mvreid/core/data/market1501.py
+++ src/mvreid/core/data/market1501.py
@@ -1,17 +1,18 @@
-import torch
-
 from mvreid.core._typing import ReidSample
 from mvreid.core.data.base import MultiViewReidDataset
 
-EntityId = int
-CameraId = int
+type EntityId = int
+"""The type of the entity ID in the Market-1501 dataset. You may change according to the actual data."""
+
+type CameraId = int
+"""The type of the camera ID in the Market-1501 dataset. You may change according to the actual data."""
 
 
 class Market1501Dataset(MultiViewReidDataset[EntityId, CameraId]):
     """The Market-1501 dataset class."""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, n_views: int) -> None:
+        super().__init__(n_views=n_views)
 
     def __len__(self) -> int:
         pass
